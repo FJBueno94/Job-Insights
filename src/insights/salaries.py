b6649ce7b5c0a8c6d03c4670bf0b1cb085ea24c2
FJBueno94 <fernando_jordi@hotmail.com>
2022-12-06 19:47:30 -0300

    adiciona a função get_min_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -11,20 +11,10 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
+    file = read(path)
+    return min([
+        int(job['min_salary']) for job in file if(job['min_salary']).isdigit()
+        ])
     raise NotImplementedError
 
 
