da09722fac949cd32aab42d80c3ba415d2c81d01
FJBueno94 <fernando_jordi@hotmail.com>
2022-12-06 16:17:58 -0300

    adiciona a função read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,21 +1,14 @@
 from functools import lru_cache
 from typing import List, Dict
 
+import csv
+
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
+    with open(path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return list(reader)
     raise NotImplementedError
 
 
