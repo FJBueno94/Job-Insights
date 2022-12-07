b31dcdee2e8d983ad407c0cc75fd0bfefb0f8ce1
FJBueno94 <fernando_jordi@hotmail.com>
2022-12-07 19:02:50 -0300

    adiciona os testes para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+import pytest
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0] == {
+        'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'
+    }
+
+
+def test_brazilian_jobs_fail():
+    with pytest.raises(FileNotFoundError, match="File not found"):
+        read_brazilian_file("tests/mocks/test_fail.csv")
