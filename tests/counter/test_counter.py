352f8ebb90f760305bd00b301bd607f965f7112c
FJBueno94 <fernando_jordi@hotmail.com>
2022-12-07 18:18:40 -0300

    adiciona os testes para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
-def test_counter():
-    pass
+def test_counter_python():
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+
+
+def test_counter_javascript():
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
+
+
+def test_counter_fail():
+    with pytest.raises(FileNotFoundError):
+        assert count_ocurrences("data/jobs.csv", "falha")
